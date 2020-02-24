1ce4196e8b6e5fb196fb0658ba8ce474937a3178
Alex <ilyushchenko@parma.ru>
2020-02-24 18:29:26 +0300

    Missed files

---
+++ src/main/java/com/champ586/fraudscore/FraudScoreApplication.java
@@ -0,0 +1,13 @@
+package com.champ586.fraudscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudScoreApplication.class, args);
+	}
+
+}
